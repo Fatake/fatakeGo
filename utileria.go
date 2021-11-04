f14a84f849cebac3ac00676d86d96ff5dcf9f909
Fatake <pace_lozano@hotmail.com>
2021-11-04 10:44:11 -0600

    cleanScreen Update

--- utileria.go
+++ utileria.go
@@ -4,13 +4,33 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 )
+
+var clear map[string]func()
+
+func init() {
+	clear = make(map[string]func())
+	clear["linux"] = func() {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
 
 // CleanScreen Funcion que limpia la pantalla
 func CleanScreen() {
-	c := exec.Command("clear")
-	c.Stdout = os.Stdout
-	c.Run()
+	value, ok := clear[runtime.GOOS]
+	if ok {
+		value()
+	} else {
+		panic("[!] No puedo limpiar la pantalla en: " + runtime.GOOS)
+	}
 }
 
 func HolaFatake() {
